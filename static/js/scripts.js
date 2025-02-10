7ba035e8e9e44ab9d731990e2e63b3b15ef2e76e
jiaoxiyun <xiyunjiao@gmail.com>
2025-02-10 23:33:13 +0800

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'What's in lab?']
+const section_names = ['home', 'publications', 'inlab']
 
 
 window.addEventListener('DOMContentLoaded', event => {
